2325ab2a5445c37034081a94fd1b066522d82c4d
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-10 19:15:40 -0400

    env added to ignore

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const express = require(express);
+const express = require('express');
+require('dotenv').config();
+// or require('dotenv/config')
 const app = express();
 
 app.get((req, res) => {
